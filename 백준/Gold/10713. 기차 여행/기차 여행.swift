dd70fefc99576807ab171fc14e9cad848a9d6d49
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-07-05 18:19:34 +0900

    [Gold V] Title: 기차 여행, Time: 220 ms, Memory: 88492 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10713. 기차 여행/기차 여행.swift
+++ 백준/Gold/10713. 기차 여행/기차 여행.swift
@@ -1,44 +1,41 @@
 import Foundation
 
-struct Rail {
-    let ticketCost: Int
-    let icCost: Int
-    let icCardCost: Int
+func readInput() -> (Int, Int, [Int], [[Int]]) {
+    let firstLine = readLine()!.split(separator: " ").map { Int($0)! }
+    let n = firstLine[0]
+    let m = firstLine[1]
+    
+    let plan = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    var cost = [[Int]]()
+    for _ in 0..<n - 1 {
+        let costLine = readLine()!.split(separator: " ").map { Int($0)! }
+        cost.append(costLine)
+    } 
+    return (n, m, plan, cost)
 }
 
-func minCost(n: Int, m: Int, route: [Int], costs: [Rail]) -> Int {
-    var totalCost = 0
-    var icCardUsed = [Bool](repeating: false, count: n - 1)
-    var icCardPurchased = [Bool](repeating: false, count: n - 1)
-    for day in 1..<m {
-        let start = route[day - 1] - 1
-        let end = route[day] - 1
-        let (s, e) = start < end ? (start, end) : (end, start)  
-        for i in s..<e {
-            if !icCardPurchased[i] {
-                if costs[i].ticketCost > costs[i].icCost + costs[i].icCardCost {
-                    totalCost += costs[i].icCardCost
-                    icCardPurchased[i] = true
-                }
-            }
-            totalCost += icCardPurchased[i] ? costs[i].icCost : costs[i].ticketCost
+func calculateCost(n: Int, m: Int, plan: [Int], cost: [[Int]]) -> Int {
+    var count = [Int](repeating: 0, count: n + 1)
+    for i in 0..<m - 1 {
+        if plan[i] < plan[i + 1] {
+            count[plan[i]] += 1
+            count[plan[i + 1]] -= 1
+        } else {
+            count[plan[i + 1]] += 1
+            count[plan[i]] -= 1
         }
     }
-    return totalCost
+
+    var sum = 0
+    var answer = 0
+    for i in 0..<n - 1 {
+        sum += count[i + 1]
+        answer += min(cost[i][0] * sum, cost[i][1] * sum + cost[i][2])
+    }
+    return answer
 }
 
-let firstLine = readLine()!.split(separator: " ").map { Int($0)! }
-let n = firstLine[0]
-let m = firstLine[1]
-let route = readLine()!.split(separator: " ").map { Int($0)! }
-var costs = [Rail]()
-for _ in 0..<n - 1 {
-    let costLine = readLine()!.split(separator: " ").map { Int($0)! }
-    let ticketCost = costLine[0]
-    let icCost = costLine[1]
-    let icCardCost = costLine[2]
-    costs.append(Rail(ticketCost: ticketCost, icCost: icCost, icCardCost: icCardCost))
-}
-
-let result = minCost(n: n, m: m, route: route, costs: costs)
+let (n, m, plan, cost) = readInput()
+let result = calculateCost(n: n, m: m, plan: plan, cost: cost)
 print(result)
