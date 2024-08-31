e090c70767fbc2cdac77fff722bf7ab726b63699
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-09-01 00:35:34 +0900

    [level 2] Title: 양궁대회, Time: 1.07 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 0.88 ms
+메모리: 16.3 MB, 시간: 1.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 06:06:48
+2024년 09월 01일 00:35:29
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.swift
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.swift
@@ -1,43 +1,36 @@
 import Foundation
 
-func solution(_ n:Int, _ info:[Int]) -> [Int] {
-    var max_diff:Int = 0
-    var max_lst:[Int] = []    
-    var dfs = [(0,0,0,0,Array(repeating: 0, count:11))]
-    
-    while dfs.count > 0 {
-        let now = dfs.removeLast()
-        let ascore = now.0
-        let lscore = now.1
-        let idx = now.2
-        let cnt = now.3
-        var lst = now.4
+func solution(_ n: Int, _ info: [Int]) -> [Int] {
+    var maxDiff = 0
+    var bestResult = [-1]
+    var dfsStack = [(aScore: 0, lScore: 0, idx: 0, usedArrows: 0, lionShots: Array(repeating: 0, count: 11))]
+
+    while !dfsStack.isEmpty {
+        let current = dfsStack.removeLast()
+        let (aScore, lScore, idx, usedArrows, lionShots) = current
         
-        if cnt > n {
-            continue    
+        if usedArrows > n {
+            continue
         }
-        
-        if idx > 10 {
-            let diff = lscore - ascore
-            if diff > max_diff {
-                lst[10] = n - cnt
-                max_lst = lst
-                max_diff = diff
+
+        if idx == 11 {
+            let scoreDiff = lScore - aScore
+            if scoreDiff > maxDiff {
+                maxDiff = scoreDiff
+                bestResult = lionShots
+                bestResult[10] += (n - usedArrows)
             }
             continue
         }
-        
-        if info[10-idx] > 0 {
-            dfs.append((ascore+idx, lscore, idx+1, cnt, lst))
-        } else {
-            dfs.append((ascore, lscore, idx+1, cnt, lst))
+
+        dfsStack.append((aScore + (info[10 - idx] > 0 ? idx : 0), lScore, idx + 1, usedArrows, lionShots))
+
+        if usedArrows + info[10 - idx] + 1 <= n {
+            var newShots = lionShots
+            newShots[10 - idx] = info[10 - idx] + 1
+            dfsStack.append((aScore, lScore + idx, idx + 1, usedArrows + newShots[10 - idx], newShots))
         }
-        
-        if cnt < n {
-            lst[10-idx] = info[10-idx] + 1
-            dfs.append((ascore, lscore+idx, idx+1, cnt+info[10-idx]+1, lst))
-        }
-    }    
-    
-    return max_diff == 0 ? [-1] : max_lst
+    }
+
+    return bestResult
 }
