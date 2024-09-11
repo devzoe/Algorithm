dbea1f127f209614630699b679ad2222d64e75b3
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-09-12 01:30:23 +0900

    [Gold II] Title: 도토리 숨기기, Time: 24 ms, Memory: 69700 KB -BaekjoonHub

--- 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.swift
+++ 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.swift
@@ -1,44 +1,40 @@
-import Foundation
+let line = readLine()!.split(separator: " ").map { Int($0)! }
+let N = line[0], K = line[1], D = line[2]
 
-func readInput() -> (Int, Int, Int, [(Int, Int, Int)]) {
-    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
-    let N = line[0], K = line[1], D = line[2]
-    
-    var list = [(Int, Int, Int)]()
-    for _ in 0..<K {
-        let q = readLine()!.split(separator: " ").map { Int(String($0))! }
-        list.append((q[0], q[1], q[2]))
-    }
-    
-    list.sort { $0.0 < $1.0 }
-    return (N, K, D, list)
+var rule: [(start: Int, end: Int, step: Int)] = []
+
+for _ in 0..<K {
+    let line = readLine()!.split(separator: " ").map { Int($0)! }
+    rule.append((line[0], line[1], line[2]))
 }
 
-func solution(_ N: Int, _ K: Int, _ D: Int, _ list: [(Int, Int, Int)]) {
-    var start = 1
-    var end = N
-    var result = 0
+var start = 1
+var end = N
+var answer = 0
 
-    while start <= end {
-        let mid = (start + end) / 2
-        var sum = 0
-        
-        for i in 0..<K {
-            let (l, r, step) = list[i]
-            if l > mid { break }
-            sum += ((min(r, mid) - l) / step + 1)
-            if sum >= D { break }
-        }
-        
-        if sum >= D {
-            result = mid
-            end = mid - 1
+while start <= end {
+    let mid = (start + end) / 2
+    var total = 0
+    
+    for r in rule {
+        if r.end < mid {
+            total += (r.end - r.start) / r.step + 1
+        } else if r.start > mid {
+            continue
         } else {
-            start = mid + 1
+            total += (mid - r.start) / r.step + 1
+        }
+        if total >= D {
+            break
         }
     }
-    print(result)
+    
+    if total >= D {
+        answer = mid
+        end = mid - 1
+    } else {
+        start = mid + 1
+    }
 }
 
-let (N, K, D, list) = readInput()
-solution(N, K, D, list)
+print(answer)
