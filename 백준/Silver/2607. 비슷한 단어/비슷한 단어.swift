349a82c5eb69dcebd18716006ad500129f147301
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-09-20 02:51:05 +0900

    [Silver II] Title: 비슷한 단어, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.swift
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.swift
@@ -1,53 +1,28 @@
 import Foundation
 
-func readInput() -> (String, [String]) {
-    let n = Int(readLine()!)!
-    let target = readLine()!
-    var words: [String] = []
-    for _ in 0..<n-1 {
-        words.append(readLine()!)
-    }
-    return (target, words)
-}
-
-func count(_ word: String) -> [Character: Int] {
-    var counts: [Character: Int] = [:]
-    for char in word {
-        counts[char, default: 0] += 1
-    }
-    return counts
-}
-
-func solution(_ word1: String, _ word2: String) -> Bool {
-    let counts1 = count(word1)
-    let counts2 = count(word2)
+func main() {
+    let N = Int(readLine()!)!
+    let target = Array(readLine()!)
+    var answer = 0
     
-    if counts1 == counts2 {
-        return true
-    }
-    
-    if abs(word1.count - word2.count) > 1 {
-        return false
-    }
-    
-    let allChars = Set(counts1.keys).union(counts2.keys)
-    var differences = 0
-    
-    for char in allChars {
-        let diff = abs((counts1[char] ?? 0) - (counts2[char] ?? 0))
-        differences += diff
-        if differences > 2 {
-            return false
+    for _ in 0..<(N-1) {
+        var compare = target
+        let word = Array(readLine()!)
+        var cnt = 0
+        
+        for w in word {
+            if let index = compare.firstIndex(of: w) {
+                compare.remove(at: index)
+            } else {
+                cnt += 1
+            }
+        }
+        
+        if cnt < 2 && compare.count < 2 {
+            answer += 1
         }
     }
-    
-    return true
-}
 
-func main() {
-    let (target, words) = readInput()
-    let result = words.filter { solution(target, $0) }.count
-    print(result)
+    print(answer)
 }
-
 main()
