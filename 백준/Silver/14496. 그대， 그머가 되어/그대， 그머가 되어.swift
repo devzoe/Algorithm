a00bf075175e821001db21be271a93e2691bffc4
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-09-21 15:02:08 +0900

    [Silver II] Title: 그대, 그머가 되어, Time: 20 ms, Memory: 79780 KB -BaekjoonHub

--- 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.swift
+++ 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.swift
@@ -1,51 +1,35 @@
 import Foundation
 
-func bfs(_ start: Int, _ target: Int, _ graph: [[Int]], _ n: Int) -> Int {
-    var queue = [(start, 0)]
-    var visited = [Bool](repeating: false, count: n + 1)
+func solution(_ n: Int, _ start: Int, _ target: Int, _ graph: [[Int]]) -> Int {
+    var queue: [(Int,Int)] = [(start, 0)]
+    var visited: [Bool] = [Bool](repeating: false, count: n+1)
     visited[start] = true
-    
     while !queue.isEmpty {
-        let (current, count) = queue.removeFirst()
-        
-        if current == target {
-            return count
-        }
-        
-        for next in graph[current] {
+        let (curr, cnt) = queue.removeFirst()
+        if curr == target { return cnt }
+        for next in graph[curr] {
             if !visited[next] {
                 visited[next] = true
-                queue.append((next, count + 1))
+                queue.append((next, cnt+1))
             }
         }
     }
-    
     return -1
 }
 
 func main() {
-    let firstLine = readLine()!.split(separator: " ").map { Int($0)! }
-    let a = firstLine[0]
-    let b = firstLine[1]
-    
-    let secondLine = readLine()!.split(separator: " ").map { Int($0)! }
-    let n = secondLine[0]
-    let m = secondLine[1]
-    
-    var graph = [[Int]](repeating: [], count: n + 1)
-    
+    let ab = readLine()!.split(separator: " ").map { Int($0)! }
+    let a = ab[0], b = ab[1]
+    let nm = readLine()!.split(separator: " ").map { Int($0)! }
+    let n = nm[0], m = nm[1]
+    var graph: [[Int]] = [[Int]](repeating: [], count: n+1)
     for _ in 0..<m {
-        let pair = readLine()!.split(separator: " ").map { Int($0)! }
-        let u = pair[0]
-        let v = pair[1]
-
-        graph[u].append(v)
-        graph[v].append(u)
+        let line = readLine()!.split(separator: " ").map { Int($0)! }
+        graph[line[0]].append(line[1])
+        graph[line[1]].append(line[0])
     }
-    
-    let result = bfs(a, b, graph, n)
-    
+    let result = solution(n,a,b,graph)
     print(result)
+    
 }
-
 main()
