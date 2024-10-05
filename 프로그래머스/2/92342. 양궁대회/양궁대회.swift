7b6ecca9ff9b6ba2c03abd47dff7a924954b1c77
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-10-05 19:19:58 +0900

    [level 2] Title: 양궁대회, Time: 1.10 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.swift
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.swift
@@ -1,36 +1,34 @@
 import Foundation
 
 func solution(_ n: Int, _ info: [Int]) -> [Int] {
-    var maxDiff = 0
-    var bestResult = [-1]
-    var dfsStack = [(aScore: 0, lScore: 0, idx: 0, usedArrows: 0, lionShots: Array(repeating: 0, count: 11))]
+    var max = 0
+    var best = [-1]
+    var stack = [(a: 0, l: 0, idx: 0, arrow: 0, shot: Array(repeating: 0, count: 11))]
 
-    while !dfsStack.isEmpty {
-        let current = dfsStack.removeLast()
-        let (aScore, lScore, idx, usedArrows, lionShots) = current
-        
-        if usedArrows > n {
+    while !stack.isEmpty {
+        let current = stack.removeLast()
+        let (a, l, idx, arrow, shot) = current
+        if arrow > n {
             continue
         }
-
         if idx == 11 {
-            let scoreDiff = lScore - aScore
-            if scoreDiff > maxDiff {
-                maxDiff = scoreDiff
-                bestResult = lionShots
-                bestResult[10] += (n - usedArrows)
+            let score = l - a
+            if score > max {
+                max = score
+                best = shot
+                best[10] += (n - arrow)
             }
             continue
         }
 
-        dfsStack.append((aScore + (info[10 - idx] > 0 ? idx : 0), lScore, idx + 1, usedArrows, lionShots))
+        stack.append((a + (info[10 - idx] > 0 ? idx : 0), l, idx + 1, arrow, shot))
 
-        if usedArrows + info[10 - idx] + 1 <= n {
-            var newShots = lionShots
-            newShots[10 - idx] = info[10 - idx] + 1
-            dfsStack.append((aScore, lScore + idx, idx + 1, usedArrows + newShots[10 - idx], newShots))
+        if arrow + info[10 - idx] + 1 <= n {
+            var newShot = shot
+            newShot[10 - idx] = info[10 - idx] + 1
+            stack.append((a, l + idx, idx + 1, arrow + newShot[10 - idx], newShot))
         }
     }
 
-    return bestResult
+    return best
 }
