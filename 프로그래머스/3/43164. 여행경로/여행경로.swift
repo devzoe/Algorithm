4e6f196ede2cd2e2b0b0cc5f5e94f41cb1975cf4
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-11-03 09:34:27 +0900

    [level 3] Title: 여행경로, Time: 0.28 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.swift
+++ 프로그래머스/3/43164. 여행경로/여행경로.swift
@@ -1,45 +1,52 @@
-import Foundation
-
-func dfs(_ current: String, _ result: inout [String], _ route: inout [String: [String]], _ visited: inout [[String]: Int], _ ticketsCount: Int) -> Bool {
-    result.append(current)
-
-    if result.count == ticketsCount + 1 {
-        return true
-    }
-
-    if let nextDestinations = route[current] {
-        for i in 0..<nextDestinations.count {
-            let next = nextDestinations[i]
-
-            if visited[[current, next]]! > 0 {
-                visited[[current, next]]! -= 1
-                if dfs(next, &result, &route, &visited, ticketsCount) {
-                    return true
-                }
-                visited[[current, next]]! += 1
-            }
-        }
-    }
-
-    result.removeLast()
-    return false
-}
-
-func solution(_ tickets: [[String]]) -> [String] {
-    var route: [String: [String]] = [:]
-    var visited: [[String]: Int] = [:]
-
-    for t in tickets {
-        route[t[0], default: []].append(t[1])
-        visited[t, default: 0] += 1
-    }
-
-    for key in route.keys {
-        route[key]?.sort()
+// 시간복잡도 줄여서 성공한 코드
+func solution(_ tickets:[[String]]) -> [String] {
+    
+    var result = [String]()
+    let total = tickets.count
+    
+    // 1. 그래프 생성
+    var graph = [String: [String]]()
+    for ticket in tickets {
+        graph[ticket[0], default: []].append(ticket[1])
     }
     
-    var result: [String] = []
-    let start = "ICN"
-    dfs(start, &result, &route, &visited, tickets.count)
+    // 우선순위 정렬하기
+    for (k,v) in graph {
+        graph[k] = graph[k]!.sorted()
+    }
+    
+    var flag = false
+    
+    func backtracking(_ cur_country: String, _ ans: [String]) {
+        if flag {
+            return
+        }
+        
+        if ans.count == total + 1 {
+            result = ans
+            flag = true
+            return
+        }
+            
+        let country_list = graph[cur_country, default: []]
+        if country_list.count == 0 { return }
+        
+        var temp = [String]()
+        temp = country_list
+        
+        for (i, next_country) in temp.enumerated() {
+            var next_country = temp[i]
+            temp.remove(at: i)
+            graph[cur_country] = temp
+            
+            backtracking(next_country, ans + [next_country])
+            
+            temp = country_list
+            graph[cur_country] = temp
+        }
+    }
+    
+    backtracking("ICN", ["ICN"])
+    
     return result
 }
