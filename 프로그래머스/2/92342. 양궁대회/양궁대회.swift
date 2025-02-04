32cfff21999cda1829ad5b14952e2c702f0d24f0
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-02-05 00:12:22 +0900

    [level 2] Title: 양궁대회, Time: 10.64 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.swift
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.swift
@@ -1,34 +1,66 @@
 import Foundation
 
-func solution(_ n: Int, _ info: [Int]) -> [Int] {
-    var max = 0
-    var best = [-1]
-    var stack = [(a: 0, l: 0, idx: 0, arrow: 0, shot: Array(repeating: 0, count: 11))]
-
-    while !stack.isEmpty {
-        let current = stack.removeLast()
-        let (a, l, idx, arrow, shot) = current
-        if arrow > n {
-            continue
-        }
-        if idx == 11 {
-            let score = l - a
-            if score > max {
-                max = score
-                best = shot
-                best[10] += (n - arrow)
+func check(_ n: Int, _ i: Int, _ r: Int, _ pscore: Int, _ lscore: Int, _ info: [Int], _ temp: inout [Int], _ max: inout Int, _ l: inout [Int]) {
+    if i == temp.count {
+        if lscore-pscore == max {
+            for k in stride(from:l.count-1,through:0,by:-1) {
+                if temp[k] > l[k] {
+                    max = lscore-pscore
+                    l = temp
+                    return
+                } else if temp[k] == l[k] {
+                    continue
+                } else {
+                    return
+                }
             }
-            continue
+        } else if lscore-pscore > max {
+            max = lscore-pscore
+            l = temp
         }
-
-        stack.append((a + (info[10 - idx] > 0 ? idx : 0), l, idx + 1, arrow, shot))
-
-        if arrow + info[10 - idx] + 1 <= n {
-            var newShot = shot
-            newShot[10 - idx] = info[10 - idx] + 1
-            stack.append((a, l + idx, idx + 1, arrow + newShot[10 - idx], newShot))
+        return
+    }
+    if r+info[i]+1 > n {
+        var p = 0
+        if info[i] > 0 { p = 10-i }
+        check(n,i+1,r,pscore+p,lscore,info,&temp,&max,&l)
+    } else {
+        temp[i] = info[i]+1
+        check(n,i+1,r+info[i]+1,pscore,lscore+(10-i),info,&temp,&max,&l)
+        temp[i] = 0
+        check(n,i+1,r,pscore+(10-i),lscore,info,&temp,&max,&l)
+        if info[i] == 0 {
+            check(n,i+1,r,pscore,lscore,info,&temp,&max,&l)
         }
     }
+}
 
-    return best
+func solution(_ n: Int, _ info: [Int]) -> [Int] {
+    var max = Int.min
+    var l = [Int](repeating:0,count:info.count)
+    
+    for i in 0..<info.count {
+        var r = 0
+        var pscore = 0
+        for j in 0..<i { pscore += (10-i)}
+        var lscore = 0
+        var temp = [Int](repeating:0,count:info.count)
+        check(n,i,r,pscore,lscore,info,&temp,&max,&l)
+    }
+    var ps = 0
+    var ls = 0
+    var cnt = 0
+    for i in 0..<l.count {
+        if l[i] > info[i] { 
+            ls += 10-i 
+            cnt += l[i]
+        }
+        else if l[i] < info[i] { ps += 10-i }
+    }
+    var result: [Int] = [-1]
+    if ls > ps { 
+        result = l 
+        if n - cnt > 0 { result[result.endIndex-1] = n - cnt }
+    }
+    return result
 }
