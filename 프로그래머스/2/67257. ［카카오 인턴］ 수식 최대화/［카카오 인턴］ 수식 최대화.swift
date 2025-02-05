b233207acd02468ca4cba5acc95a9b46d25a27c2
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-02-05 21:44:47 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.91 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.swift
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.swift
@@ -1,70 +1,72 @@
 import Foundation
 
-func permutations<T>(_ array: [T], _ k: Int) -> [[T]] {
-    var result: [[T]] = []
-
-    func backtracking(_ ans: inout [T], _ visited: inout [Bool]) {
-        if ans.count == k {
+func permutation(_ arr: [String]) -> [[String]] {
+    var result: [[String]] = []
+    func backtrack(_ curr: Int, _ ans: inout [String]) {
+        if ans.count == arr.count {
             result.append(ans)
             return
         }
-
-        for i in 0..<array.count {
-            if visited[i] {
-                continue
+        for i in 0..<arr.count {
+            if !ans.contains(arr[i]) {
+                ans.append(arr[i])
+                backtrack(curr+1, &ans)
+                ans.removeLast()
             }
-
-            visited[i] = true
-            ans.append(array[i])
-            backtracking(&ans, &visited)
-            ans.removeLast()
-            visited[i] = false
         }
     }
-
-    var ans: [T] = []
-    var visited = Array(repeating: false, count: array.count)
-    backtracking(&ans, &visited)
+    var ans: [String] = []
+    backtrack(0,&ans)
     return result
 }
-
 func solution(_ expression:String) -> Int64 {
-    let originalOperand = expression.components(separatedBy: ["+","-","*"]).map{ Int($0)! }
-    let originalOps = Array(expression.filter { !($0.isNumber) })
-    let op = Array(Set(originalOps))
-    var maxNum = -1
-    
-    let permu = permutations(op, op.count)
-    
-    for pm in permu {
-        var operand = originalOperand
-        var ops = originalOps
-        
-        for p in pm {
+    let op = ["+","*","-"]
+    var opr: [String] = []
+    expression.forEach { 
+        if op.contains(String($0)) {
+            opr.append(String($0))
+        }
+    }
+    var opSet: Set<String> = Set(opr)
+    var n = expression
+    for o in opSet {
+        n = n.replacingOccurrences(of: o, with: " ")
+    }
+    var num = n.split(separator: " ").map { Int($0)! }
+    let order = permutation(Array(opSet))
+    var maxValue = Int.min
+    for o in order {
+        var tempOp = opr
+        var tempNum = num
+        for item in o {
             var i = 0
-            while i < ops.count {
-                var n = 0
-                if ops[i] == p {
-                    switch p {
+            var cnt = 0
+            let l = tempNum.count
+            while cnt < l {
+                if i >= tempOp.count { break } 
+                cnt += 1
+                if tempOp[i] == item {
+                    var temp = 0
+                    switch item {
                     case "+":
-                        n = operand[i] + operand[i+1]
-                    case "-":
-                        n = operand[i] - operand[i+1]
+                        temp = tempNum[i] + tempNum[i+1]      
                     case "*":
-                        n = operand[i] * operand[i+1]
+                        temp = tempNum[i] * tempNum[i+1]
+                    case "-":
+                          temp = tempNum[i] - tempNum[i+1]  
                     default:
                         break
                     }
-                
-                    operand[i] = n
-                    operand.remove(at: i+1)
-                    ops.remove(at: i)
-                } else {
-                    i += 1
+                    tempNum[i+1] = temp
+                    tempOp.remove(at:i)
+                    tempNum.remove(at:i)
+                    continue
                 }
+                i += 1
             }
         }
-        maxNum = max(abs(operand[0]), maxNum)
+        tempNum = tempNum.map { abs($0) }
+        maxValue = max(maxValue, tempNum.max()!)
     }
-    return Int64(maxNum)
+    return Int64(maxValue)
 }
