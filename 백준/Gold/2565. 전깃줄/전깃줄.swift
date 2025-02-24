d0c1b2ad46b2a24eb0f0919ba265e06894dc2d11
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-02-24 16:37:09 +0900

    [Gold V] Title: 전깃줄, Time: 8 ms, Memory: 79520 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.swift
+++ 백준/Gold/2565. 전깃줄/전깃줄.swift
@@ -1,26 +1,22 @@
 import Foundation
 
 let n = Int(readLine()!)!
-var pairs = [(Int, Int)]()   
+var arr: [[Int]] = []
+var arr2: [Int] = []
+var dp: [Int] = [Int](repeating: 1, count: n)
 
 for _ in 0..<n {
-    let line = readLine()!.split(separator:" ").map { Int($0)! }
-    pairs.append((line[0], line[1]))
+    let line = readLine()!.split(separator: " ").map { Int($0)! }
+    arr.append(line)
 }
-    
-pairs.sort { $0.0 < $1.0 }
-    
-var dp = [Int](repeating: 0, count: n)
-var answer = 0
-    
+arr.sort { $0[0] < $1[0] }
+arr.forEach { arr2.append($0[1])}
+dp[0] = 1
 for i in 0..<n {
-    dp[i] = 1
     for j in 0..<i {
-        if pairs[j].1 < pairs[i].1, dp[j] + 1 > dp[i] {
-            dp[i] = dp[j] + 1
+        if arr2[j] < arr2[i] {
+            dp[i] = max(dp[j] + 1,dp[i])
         }
     }
-    answer = max(answer, dp[i])
 }
-
-print(n - answer)
+print(n-dp.max()!)
