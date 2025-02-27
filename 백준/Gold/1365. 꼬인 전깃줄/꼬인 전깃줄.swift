940dbb9c09918c6ada57723377bb6e41f516b2db
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-02-27 18:59:05 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 44 ms, Memory: 87292 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var arr: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
+var lis: [Int] = [Int](repeating: 0, count: n)
+func binarySearch(_ left: Int, _ right: Int, _ target: Int) -> Int {
+    var l = left
+    var r = right
+    while l < r {
+        let mid = (l+r)/2
+        if lis[mid] < target {
+            l = mid + 1
+        } else {
+            r = mid
+        }
+    }
+    return r
+}
+var j = 0
+lis[0] = arr[0]
+for i in 1..<n {
+    if lis[j] < arr[i] {
+        lis[j+1] = arr[i]
+        j += 1
+    } else {
+        let idx = binarySearch(0, j, arr[i])
+        lis[idx] = arr[i]
+    }
+}
+print(n-j-1)
