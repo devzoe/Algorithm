b96d2a833e0b4da2b1e93629db74b22d1c6ccd6f
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-03-28 05:03:47 +0900

    [Silver I] Title: IOIOI, Time: 64 ms, Memory: 96124 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.swift
+++ 백준/Silver/5525. IOIOI/IOIOI.swift
@@ -3,22 +3,20 @@ import Foundation
 let n = Int(readLine()!)!
 let m = Int(readLine()!)!
 let s = Array(readLine()!)
-var ioi = "IOI"
-for _ in 1..<n {
-    ioi += "OI"
-}
-let len = ioi.count
-var fixed = ""
-for i in 0..<len {
-    fixed += String(s[i])
-}
 var result = 0
-if fixed == ioi { result += 1 }
-for i in len..<s.count {
-    fixed.removeFirst()
-    fixed += String(s[i])
-    if fixed == ioi {
-        result += 1
+var cnt = 0
+var i = 0
+while i+2 < m {
+    if s[i] == "I" && s[i+1] == "O" && s[i+2] == "I" {
+        i += 2
+        cnt += 1
+        if cnt == n {
+            result += 1
+            cnt -= 1
+        }
+    } else {
+        i += 1
+        cnt = 0
     }
 }
 print(result)
