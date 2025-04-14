4de9fae7689b992f742a64fe21e13ad4357b6610
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-04-15 03:00:27 +0900

    [Silver I] Title: 트리 순회, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.swift
+++ 백준/Silver/1991. 트리 순회/트리 순회.swift
@@ -1,47 +1,19 @@
 let n = Int(readLine()!)!
-var tree: [String] = [String](repeating: "", count: 134217728)
-tree[1] = "A"
+var tree: [String:[String]] = [:]
+var ordered = ["","",""]
 for _ in 0..<n {
     let ch = readLine()!.split(separator: " ").map { String($0) }
-    for i in 1..<134217728 {
-        if tree[i] == ch[0] {
-            if ch[1] != "." {
-                tree[i*2] = ch[1]
-            }
-            if ch[2] != "." {
-                tree[i*2+1] = ch[2]
-            }
-            break
-        }
-    }
-    
+    tree[ch[0],default: []].append(contentsOf: [ch[1],ch[2]])
 }
-func preorder(_ curr: Int) {
-    if tree[curr] == "" {
-        return
-    }
-    print(tree[curr],terminator: "")
-    preorder(curr*2)
-    preorder(curr*2+1)
+func order(_ curr: String) {
+    if curr == "." { return }
+    ordered[0] += curr
+    order(tree[curr]![0])
+    ordered[1] += curr
+    order(tree[curr]![1])
+    ordered[2] += curr
 }
-func inorder(_ curr: Int) {
-    if tree[curr] == "" {
-        return
-    }
-    inorder(curr*2)
-    print(tree[curr],terminator: "")
-    inorder(curr*2+1)
+order("A")
+for i in ordered {
+    print(i)
 }
-func postorder(_ curr: Int) {
-    if tree[curr] == "" {
-        return
-    }
-    postorder(curr*2)
-    postorder(curr*2+1)
-    print(tree[curr],terminator: "")
-}
-preorder(1)
-print()
-inorder(1)
-print()
-postorder(1)
